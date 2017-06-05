a738c7b1c25a4aad5b9182545af816b4d99a286a
Sam Burns <samburns157@gmail.com>
2017-06-05 11:21:44 -0700

    Makefile changes to support subdirs in source tree

--- src/kernel/include/structures/queue.h
+++ src/kernel/include/structures/queue.h
@@ -1,10 +1,13 @@
 #include <stdint.h>
+#include <stddef.h>
 #pragma once
 
-struct kps2_queue
+typedef struct kqueue_s
 {
-	(void *)elements;
+	void * elements;
 	size_t element_size;
 	uint16_t count;
 	uint16_t max_count;
-}
+} kqueue;
+
+void kqueue_init(kqueue * queue, size_t element_size, uint16_t max_count);
--- src/kernel/src/structures/queue.c
+++ src/kernel/src/structures/queue.c
@@ -1,2 +1,9 @@
 #include "structures/queue.h"
 
+void kqueue_init(kqueue * queue, size_t element_size, uint16_t max_count)
+{
+	queue->elements = (void *)0x1000;
+	queue->element_size = element_size;
+	queue->count = 0;
+	queue->max_count = max_count;
+}
